6dbe56808bb6c48132feadb62ba8bf7f63d1b40b
Unknown <smorrisey@gatech.edu>
2019-04-04 10:30:26 -0400

    Question 4
    Question 4 is done other than missing the audio file

--- ass4main.m
+++ ass4main.m
@@ -1,6 +1,6 @@
 % noise generation from assignment 3
 % needs word length defined
-w = 4;
+w = 8;
 d = 1/(2^(w-1)); 
 R = [-d/2 d/2];     
 n = rand(44100,1) .* range(R) + min(R); 
@@ -9,18 +9,41 @@ n = rand(44100,1) .* range(R) + min(R);
 % Question 3
 noiseMyFilter = mySinglePole(n, 0.99);
 noiseMLFilter = filter(0.01, [1, -0.99], n);
-% find difference and plot
 
+% Plot for Question 3
 figure(1)
-subplot(2, 1, 1)
+subplot(3, 1, 1)
 plot(noiseMyFilter)
-subplot(2, 1, 2)
+title("Filtered with mySinglePole")
+subplot(3, 1, 2)
 plot(noiseMLFilter)
+title("Filtered with Matlab Filter")
+subplot(3,1,3)
+plot(noiseMLFilter - noiseMyFilter)
+title("Difference between the filters")
 
 % Question 4
-% [a, fs] = audioread("cathy_2.wav");
-% which filter is better?
-% Filter for alpha = 0.5, 0.9, 0.99, 0.999
+% missing audio file
+[a, fs] = audioread("cathy_2.wav");
+cathy05 = mySinglePole(a, 0.5);
+cathy09 = mySinglePole(a, 0.9);
+cathy099 = mySinglePole(a, 0.99);
+cathy0999 = mySinglePole(a, 0.999);
+
+% Plotting question 4
+figure(2)
+subplot(4,1,1)
+plot(cathy05)
+title("Alpha = 0.5")
+subplot(4,1,2)
+plot(cathy09)
+title("Alpha = 0.9")
+subplot(4,1,3)
+plot(cathy099)
+title("Alpha = 0.99")
+subplot(4,1,4)
+plot(cathy0999)
+title("Alpha = 0.999")
 
 % Question 6
 % freqz((1-alpha), [1  -alpha],1024,44100)
